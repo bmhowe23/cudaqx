90337c162888948b0b69279e1f5dd184df1ec12f
Ben Howe <bhowe@nvidia.com>
2025-05-01 23:32:36 +0000

    Implement decode_batch
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- libs/qec/lib/decoders/sliding_window.cpp
+++ libs/qec/lib/decoders/sliding_window.cpp
@@ -116,7 +116,7 @@ public:
     }
   }
 
-  virtual decoder_result decode(const std::vector<float_t> &syndrome) {
+  virtual decoder_result decode(const std::vector<float_t> &syndrome) override {
     // Initialize converged to true and set to false if any inner decoder
     // fails to converge.
     decoder_result outer_result{true, std::vector<float_t>(block_size, 0.0)};
@@ -192,6 +192,91 @@ public:
     }
 
     return outer_result;
+  }
+
+  virtual std::vector<decoder_result>
+  decode_batch(const std::vector<std::vector<float_t>> &syndromes) override {
+    printf("Decoding batch of size %zu\n", syndromes.size());
+    std::vector<decoder_result> results(syndromes.size());
+    for (std::size_t i = 0; i < syndromes.size(); ++i) {
+      results[i] = decode(syndromes[i]);
+    }
+    cudaqx::tensor<std::uint8_t> result_tensor(
+        std::vector<std::size_t>{syndromes.size(), this->block_size});
+    for (std::size_t w = 0; w < num_windows; ++w) {
+      std::size_t syndrome_start = w * step_size * num_syndromes_per_round;
+      std::size_t syndrome_end = syndrome_start + num_syndromes_per_window - 1;
+      std::vector<std::vector<cudaq::qec::float_t>> syndrome_slices(
+          syndromes.size());
+      for (std::size_t s = 0; s < syndromes.size(); ++s) {
+        syndrome_slices[s] = std::vector<cudaq::qec::float_t>(
+            syndromes[s].begin() + syndrome_start,
+            syndromes[s].begin() + syndrome_end + 1);
+      }
+      std::vector<cudaqx::tensor<uint8_t>> syndrome_mods(
+          syndromes.size(), std::vector<std::size_t>{this->syndrome_size});
+      if (w > 0) {
+        // Modify the syndrome slice to account for the previous windows.
+        // FIXME we can make this more efficient.
+        // syndrome_mods is syndrome_size x num_syndromes_in_batch_call
+        auto syndrome_mods = full_pcm.dot(result_tensor.transpose());
+        for (std::size_t s = 0; s < syndromes.size(); ++s) {
+          for (std::size_t r = 0; r < num_syndromes_per_window; ++r) {
+            auto &slice_val = syndrome_slices[s].at({r});
+            slice_val =
+                static_cast<double>(static_cast<std::uint8_t>(slice_val) ^
+                                    syndrome_mods.at({r + syndrome_start, s}));
+          }
+        }
+      }
+      // printf("Window %zu: syndrome_start = %zu, syndrome_end = %zu length1 = "
+      //        "%zu length2 = %zu\n",
+      //        w, syndrome_start, syndrome_end, syndrome_slice.size(),
+      //        syndrome_end - syndrome_start + 1);
+      auto inner_results = inner_decoders[w]->decode_batch(syndrome_slices);
+      // if (!inner_result.converged) {
+      //   printf("Window %zu: inner decoder failed to converge\n", w);
+      // }
+      std::vector<cudaqx::tensor<uint8_t>> window_results(syndromes.size());
+      for (std::size_t s = 0; s < syndromes.size(); ++s) {
+        results[s].converged &= inner_results[s].converged;
+        cudaq::qec::convert_vec_soft_to_tensor_hard(inner_results[s].result,
+                                                    window_results[s]);
+      }
+      const auto &window_pcm = decoder_pcms[w];
+      // printf("PCM dims: %zu x %zu, window_result dims: %zu\n",
+      //        window_pcm.shape()[0], window_pcm.shape()[1],
+      //        window_result.shape()[0]);
+      // auto result = window_pcm.dot(window_result);
+      // Commit to everything up to the first column of the next window.
+      if (w < num_windows - 1) {
+        // Prepare for the next window.
+        auto next_window_first_column = first_columns[w + 1];
+        auto this_window_first_column = first_columns[w];
+        auto num_to_commit =
+            next_window_first_column - this_window_first_column;
+        // printf("  Committing %u bits from window %zu\n", num_to_commit, w);
+        for (std::size_t s = 0; s < syndromes.size(); ++s) {
+          for (std::size_t c = 0; c < num_to_commit; ++c) {
+            result_tensor.at({s, c + this_window_first_column}) =
+                window_results[s].at({c});
+          }
+        }
+      } else {
+        // This is the last window. Append ALL of window_result to
+        // decoded_result.
+        auto this_window_first_column = first_columns[w];
+        auto num_to_commit = window_results[0].shape()[0];
+        // printf("  Committing %zu bits from window %zu\n", num_to_commit, w);
+        for (std::size_t s = 0; s < syndromes.size(); ++s) {  
+          for (std::size_t c = 0; c < num_to_commit; ++c) {
+            result_tensor.at({s, c + this_window_first_column}) =
+                window_results[s].at({c});
+          }
+        }
+      }
+    }
+    return results;
   }
 
   virtual ~sliding_window() {}
